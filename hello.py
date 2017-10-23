96982f2e32e61e640065b74b17fd5564986eb763
lucyluoxi90 <xiangluox@gmail.com>
2017-10-23 17:26:27 -0400

    python examples

---
+++ hello.py
@@ -0,0 +1,46 @@
+print("Hello World")
+
+
+the_world_is_flat = 1
+if the_world_is_flat:
+	print "Be careful not to fall off!"
+
+#3.1.1
+2+2
+50-5*6
+(50-5.0*6)/4
+8/5.0
+
+17/3 #int / int ->int
+17/3.0 #int / float -> float
+17//3.0 # explicit floor division discards the fractional part
+17%3 # the % operator returns the remainder of the division
+5*3+2 # result * divisor + remainder
+
+5**2 # 5 squared
+2**7 # 2 to the power of 7
+
+width = 20
+height = 5*9
+width*height
+
+n # try to access an undefined variable
+
+tax=12.4/100
+price= 100.50
+price*tax
+price+_
+round(_,2)
+
+
+#3.1.2
+'spam eggs' #single quotes
+
+
+
+
+
+
+
+
+
