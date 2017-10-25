986259793049b889b6f863e4c4151f146b585cc4
lucyluoxi90 <xiangluox@gmail.com>
2017-10-24 22:55:36 -0400

    updatech5

---
+++ DataStructure.py
@@ -0,0 +1,54 @@
+5.1
+#list functions
+
+a=[66.25, 333, 333, 1, 1234.5]
+print a.count(333), a.count (66.25), a.count("x")
+a.insert(2,-1)
+print a
+a.append(333)
+print a
+print a.index(333)
+a.reverse()
+print a
+a.sort()
+print a
+print a.pop()
+print a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
