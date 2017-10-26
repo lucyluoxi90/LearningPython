782c659ecbd76085f7960e83692db2eb98232d14
lucyluoxi90 <xiangluox@gmail.com>
2017-10-25 23:01:13 -0400

    updates

--- DataStructure.py
+++ DataStructure.py
@@ -15,13 +15,34 @@ print a
 print a.pop()
 print a
 
+stack = [3, 4, 5]
+stack.append(6)
+stack.append(7)
+stack  #[3, 4, 5, 6, 7]
+stack.pop()  #7
+stack.pop()  #6
+stack.pop() #5
+stack  #[3, 4]
 
 
+5.1.2
+from collections import deque
+queue = deque(["Eric", "John", "Michael"])
+queue.append("Terry")           # Terry arrives
+queue.append("Graham")          # Graham arrives
+queue.popleft()                 # The first to arrive now leaves, returns'Eric'
+queue.popleft()                 # The second to arrive now leaves, returns 'John'
+queue                           # Remaining queue in order of arrival, returns deque(['Michael', 'Terry', 'Graham'])
 
+5.1.3
 
+def f(x): return x % 3 == 0 or x % 5 == 0
 
+filter(f, range(2, 25)) #returns [3, 5, 6, 9, 10, 12, 15, 18, 20, 21, 24]
 
+def cube(x): return x*x*x
 
+map(cube, range(1, 11)) #returns [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
 
 
 
